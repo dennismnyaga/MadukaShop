42a1df1ffadffaf91374a3fcb908f953b329b570
dennismnyaga <murildennis7@gmail.com>
2023-04-26 04:32:51 +0300

    Reconfigure the static files

--- shopapp/static/shopapp/js/main.js
+++
@@ -1,7 +0,0 @@
-const collButton = document.querySelector('.coll button');
-const searcher = document.querySelector('.searcher');
-searcher.classList.add('hide'); 
-collButton.addEventListener('click', e => {
-    searcher.classList.toggle('show'); // Show the searcher when the button is clicked
-    collButton.classList.toggle('hide'); // Hide the button
-});
