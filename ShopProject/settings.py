0bae2f6a4403b650ae438c621419191bc208064e
dennismnyaga <murildennis7@gmail.com>
2023-05-06 23:14:00 +0300

    Removed the pre-lender

--- ShopProject/settings.py
+++ ShopProject/settings.py
@@ -136,24 +136,24 @@ WSGI_APPLICATION = 'ShopProject.wsgi.application'
 
 """===============================Start of Production database configuration=============================="""
 
-DATABASES = {
-    'default': {
+# DATABASES = {
+#     'default': {
 
-        'ENGINE': 'django.db.backends.postgresql',
+#         'ENGINE': 'django.db.backends.postgresql',
 
-        'NAME': 'mashop',
+#         'NAME': 'mashop',
 
-        'USER': 'postgres',
+#         'USER': 'postgres',
 
-        'PASSWORD': 'madukaat2023',
+#         'PASSWORD': 'madukaat2023',
 
-        'HOST': 'mashop.ckp6xx54qapt.eu-central-1.rds.amazonaws.com',
+#         'HOST': 'mashop.ckp6xx54qapt.eu-central-1.rds.amazonaws.com',
 
-        'PORT': '5432',
+#         'PORT': '5432',
 
-    }
+#     }
 
-}
+# }
 
 
 """=====================================End of Product database configuration============================================== """
@@ -161,25 +161,25 @@ DATABASES = {
 
 """============================Start of Local Database Configuration========================================================"""
 
-# DATABASES = {
-#     'default': {
+DATABASES = {
+    'default': {
 
-#         'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
 
-#         'NAME': 'MadukaShop',
+        'NAME': 'MadukaShop',
 
-#         'USER': 'postgres',
+        'USER': 'postgres',
 
-#         'PASSWORD': '33016460d',
+        'PASSWORD': '33016460d',
 
-#         'HOST': 'localhost',
+        'HOST': 'localhost',
 
-#         'PORT': '5432',
+        'PORT': '5432',
         
 
-#     }
+    }
 
-# }
+}
 
 
 """ =============================End of Local Database Configuration==================================== """
@@ -228,13 +228,13 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 """ =============================Production AWS Storages================================= """
 
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_QUERYSTRING_AUTH = False
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_QUERYSTRING_AUTH = False
 
-AWS_ACCESS_KEY_ID = 'AKIA53IMYZT7SVG7XVSR'
-AWS_SECRET_ACCESS_KEY = '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'
+# AWS_ACCESS_KEY_ID = 'AKIA53IMYZT7SVG7XVSR'
+# AWS_SECRET_ACCESS_KEY = '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'
 
-AWS_STORAGE_BUCKET_NAME = 'maduka-shop'
+# AWS_STORAGE_BUCKET_NAME = 'maduka-shop'
 
 
 
@@ -344,3 +344,6 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
+
+
+SEO_JS_PRERENDER_TOKEN = "123456789abcdefghijkl"  # Really, put this in your env, not your codebase.
