0a6c9556959e506e32abd20c161428944a76f305
dennismnyaga <murildennis7@gmail.com>
2023-05-28 11:06:00 +0300

    gbdsjk

--- ShopProject/settings.py
+++ ShopProject/settings.py
@@ -117,48 +117,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ShopProject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': '1cHGQ3TnjKfL8gWocriA',
-#         'HOST': 'containers-us-west-40.railway.app',
-#         'PORT': '6298',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'mydatabase',
-#     }
-# }
 
 
 """===============================Start of Production database configuration=============================="""
 
-# DATABASES = {
-#     'default': {
 
-#         'ENGINE': 'django.db.backends.postgresql',
-
-#         'NAME': 'production-maduka-db-1 ',
-
-#         'USER': 'postgres',
-
-#         'PASSWORD': 'logicaltender2023',
-
-#         'HOST': 'production-maduka-db-1.ckp6xx54qapt.eu-central-1.rds.amazonaws.com',
-
-#         'PORT': '5432',
-
-#     }
-
-# }
 DATABASES = {
     'default': {
 
