56f1dcdab109df129cc2de2759ffa68819b62689
dennismnyaga <murildennis7@gmail.com>
2024-09-05 19:31:12 +0300

    frontend code

---
+++ src/config.js
@@ -0,0 +1,19 @@
+// config.js
+const config = {
+  development: {
+    // apiUrl: "https://test.madukaonline.co.ke",
+    // apiUrl: "https://madukabackend.barakagaspoint.co.ke",
+    apiUrl: "http://127.0.0.1:8000",
+  },
+  production: {
+    // apiUrl: "https://madukabackend.barakagaspoint.co.ke",
+    // apiUrl: "https://test.madukaonline.co.ke",
+    apiUrl: "http://127.0.0.1:8000",
+  },
+  googleMaps: {
+    // apiKey: 'AIzaSyAFj9VA8r2rotB_TRqe0RNEBJGMvf3fkxQ',
+    apiKey: 'AIzaSyAwp---f_-ORxElgCjA3UKdKCIB_9fbD9Y',
+  }
+};
+
+export default config;
